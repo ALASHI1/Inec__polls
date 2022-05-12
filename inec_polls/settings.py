42965ba2a59dc4cc5dcb49d91d4cc726900c392f
ALASHI1 <alashimuyiwa@gmail.com>
2022-05-12 22:18:44 +0100

    database

--- inec_polls/settings.py
+++ inec_polls/settings.py
@@ -103,9 +103,9 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#   'default': dj_database_url.config(conn_max_age=600)
-# }
+DATABASES = {
+    'default':dj_database_url.config('mysql://ben_shi: @localhost:3306/bincomphptest')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
